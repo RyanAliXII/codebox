4195499b117cdfac2b748b0c6e56fd82dc6c84c9
Ryan Ali <ryanali456@gmail.com>
2023-04-25 18:44:03 +0800

    add port to env

--- models/Database.class.php
+++ models/Database.class.php
@@ -9,6 +9,7 @@ class Database{
     private $username;
     private $password;
     private $dbName;
+    private $port;
 
 
 
@@ -18,7 +19,8 @@ public function getConnection(){
         $this->username = $_ENV["DB_USER"];
         $this->password = $_ENV["DB_PASSWORD"];
         $this->dbName = $_ENV["DB_NAME"];
-        $connection = new PDO("mysql:host=$this->host;dbname=$this->dbName;", $this->username, $this->password);
+        $this->port = $_ENV["DB_PORT"];
+        $connection = new PDO("mysql:host=$this->host; port=$this->port; dbname=$this->dbName;", $this->username, $this->password);
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
        
         return $connection;
